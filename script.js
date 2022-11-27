70b4abd19044983c46845155f73240d7e1fd4863
Audrey Alcantara <adrianaquin22@gmail.com>
2022-11-27 15:17:33 -0500

    issues with obtaining value of city for queryurl

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 var APIKey = 'd8b4e9c1ebd7b4bfdc1c3e9a6c0207cf'
+var city
+
+function citySelected(){
+    city = document.getElementById('cities1').value
+    console.log(city)
+    //return city
+}
+
+var queryURL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`
+console.log(queryURL)
+
+/* fetch(queryURL)
+    .then(function (response) {
+        return response.json()
+    })
+    .then(function (data) {
+        console.log(data)
+    }) */
