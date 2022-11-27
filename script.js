2003dd94190e514a6f2267b10bd113fcca6b024f
Audrey Alcantara <adrianaquin22@gmail.com>
2022-11-27 16:37:46 -0500

    able to grab coords for city and put into 5 day forcast api call"

--- script.js
+++ script.js
@@ -5,17 +5,26 @@ function citySelected(){
     city = document.getElementById('cities1').value
     console.log(city)
     //return city
-    var queryURL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`
-    //console.log(queryURL)
-    fetch(queryURL)
+    var cityURL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`
+    //console.log(cityURL)
+    fetch(cityURL)
     .then(function (response){
         return response.json()
     })
     .then(function (response){
-        console.log(response)
+        console.log(response.coord)
+        var lat = response.coord.lat  // this code grabs the coordinates of the city to use in the 5 day forcast api url 
+        //console.log(lat)
+        var lon = response.coord.lon
+        //console.log(lon)
+        //return (lat, lon)
+        var queryURL = `api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIKey}`
+        console.log(queryURL)
     })
 }
 
+//var queryURL = `api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIKey}`
+//console.log(queryURL)
 
 /* 
 function citySelected(){
