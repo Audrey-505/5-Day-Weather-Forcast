2879423531da9e36b46027bafb35f698657f8048
Audrey Alcantara <adrianaquin22@gmail.com>
2022-11-29 19:44:28 -0500

    updates

--- script.js
+++ script.js
@@ -97,3 +97,50 @@ function getCoords(city) {
     })
  }
    
+//this is my test
+//var queryURL = `api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIKey}`
+//console.log(queryURL)
+
+/* 
+function citySelected(){
+    city = document.getElementById('cities1').value
+    console.log(city)
+fetch("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey)
+.then(response => {
+            \\ need logic to push search results, save to local storage, eventually append to a list of searched cities
+            return response.json()
+        })
+ .then(response => {
+            console.log(response);
+*/
+
+// function citySelected(){
+//     city = document.getElementById('cities1').value
+//     console.log(city)
+//     //return city
+//     var cityURL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`
+//     //console.log(cityURL)
+//     fetch(cityURL)
+//     .then(function (response){
+//         return response.json()
+//     })
+//     .then(function (response){
+//         console.log(response.coord)
+//         var lat = response.coord.lat  // this code grabs the coordinates of the city to use in the 5 day forcast api url 
+//         //console.log(lat)
+//         var lon = response.coord.lon
+//         //console.log(lon)
+//         //return (lat, lon)
+//         var queryURL = `api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIKey}`
+//         //console.log(queryURL)
+//         fetch(queryURL, {
+//             mode: 'cors'
+//         })
+//         .then(function (data){
+//         console.log(data.json())
+//         })
+//         // .then(function (data){
+//         //     console.log(data)
+//         // })
+//     })
+// }
