175cde133ba77e15bdd18fdb4267f80aee548501
Audrey Alcantara <adrianaquin22@gmail.com>
2022-12-03 17:10:52 -0500

    five-day forcast and icons added

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 var currentWDisplay = document.getElementById('currentDisplay')
+var futureWeatherD = document.getElementById('five-dayDisplay')
 
 var APIKey = 'd8b4e9c1ebd7b4bfdc1c3e9a6c0207cf'
 // var city 
@@ -23,7 +24,7 @@ function getCoords(city) {
 } 
 
  function getWeather(a, b){
-    var queryURL = `http://api.openweathermap.org/data/2.5/forecast?lat=${a}&lon=${b}&appid=${APIKey}`
+    var queryURL = `http://api.openweathermap.org/data/2.5/forecast?lat=${a}&lon=${b}&appid=${APIKey}&units=imperial`
     fetch(queryURL)
     .then(function (response){
     console.log(response)
@@ -32,9 +33,10 @@ function getCoords(city) {
     .then(function (data){
     console.log(data)
     currentWeather(data)
+    futureWeather(data)
     })
  }
-
+var iconURL = `https://openweathermap.org/img/wn/`
 function currentWeather(data){
 $(currentWDisplay).append(`
 <h2>Current Weather</h2>
@@ -42,22 +44,75 @@ $(currentWDisplay).append(`
 <table class="table">
             <thead>
                 <tr>
-                    <th scope="col">icon</th>
-                    <th scope="row">temp</th>
-                    <th scope="row">wind</th>
-                    <th scope="row">humidity</th>
+                    <th scope="col">Weather</th>
+                    <th scope="row">Tempreture</th>
+                    <th scope="row">Wind Speed</th>
+                    <th scope="row">Humidity</th>
                 </tr>
             </thead>
             <tbody>
                 <tr>
-                    <th scope="row">${data.list[0].weather[0].icon}</th>
+                    <th scope="row"><img src="${iconURL}${data.list[0].weather[0].icon}.png"></th>
                     <th scope="row">${data.list[0].main.feels_like}</th>
-                    <th scope="row">${data.list[0].wind.speed}</th>
-                    <th scope="row">${data.list[0].main.humidity}</th>
+                    <th scope="row">${data.list[0].wind.speed}mph</th>
+                    <th scope="row">${data.list[0].main.humidity}%</th>
                 </tr>
             </tbody>
         </table>
 `)   
+}
+
+function futureWeather(data){
+$(futureWeatherD).append(`
+<h2>5-Day Forcast</h2>
+<h3>City: ${data.city.name}</h3>
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">Date</th>
+      <th scope="col">${data.list[0].dt}</th>
+      <th scope="col">${data.list[8].dt}</th>
+      <th scope="col">${data.list[16].dt}</th>
+      <th scope="col">${data.list[24].dt}</th>
+      <th scope="col">${data.list[32].dt}</th>
+    </tr>
+  </thead>
+  <tbody>
+  <tr>
+    <th scope="row">Weather</th>
+    <td><img src="${iconURL}${data.list[0].weather[0].icon}.png"></td>
+    <td><img src="${iconURL}${data.list[8].weather[0].icon}.png"></td>
+    <td><img src="${iconURL}${data.list[16].weather[0].icon}.png"></td>
+    <td><img src="${iconURL}${data.list[24].weather[0].icon}.png"></td>
+    <td><img src="${iconURL}${data.list[32].weather[0].icon}.png"></td>
+  </tr>
+  <tr>
+    <th scope="row">Temp</th>
+    <td>${data.list[0].main.feels_like}</td>
+    <td>${data.list[8].main.feels_like}</td>
+    <td>${data.list[16].main.feels_like}</td>
+    <td>${data.list[24].main.feels_like}</td>
+    <td>${data.list[32].main.feels_like}</td>
+  </tr>
+  <tr>
+    <th>Wind</th>
+    <td>${data.list[0].wind.speed}mph</td>
+    <td>${data.list[8].wind.speed}mph</td>
+    <td>${data.list[16].wind.speed}mph</td>
+    <td>${data.list[24].wind.speed}mph</td>
+    <td>${data.list[32].wind.speed}mph</td>
+  </tr>
+  <tr>
+    <th>Humidity</th>
+    <td>${data.list[0].main.humidity}%</td>
+    <td>${data.list[8].main.humidity}%</td>
+    <td>${data.list[16].main.humidity}%</td>
+    <td>${data.list[24].main.humidity}%</td>
+    <td>${data.list[32].main.humidity}%</td>
+  </tr>
+</tbody>
+</table>
+`)
 }
 
 //this is my test test
